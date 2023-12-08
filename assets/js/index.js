f5ae2468b56e3e3c5edc7240b61af32444087acf
Emerson-Fleming <85723483+Emerson-Fleming@users.noreply.github.com>
2023-12-08 10:48:03 -0800

    school sample

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// index.js
+
+// Function to reset the gallery scroll
+function resetGalleryScroll() {
+    const galleryContainer = document.querySelector('.horizontal-scroll-gallery');
+    galleryContainer.style.animation = 'none'; // Pause the animation
+    galleryContainer.style.transform = 'translateX(0)'; // Reset the scroll position
+    setTimeout(() => {
+        galleryContainer.style.animation = 'scrollGallery 10s linear infinite'; // Resume the animation
+    }, 100); // Delay the resumption to prevent flickering
+}
+
+// Check scroll position and reset if necessary
+setInterval(function() {
+    const galleryContainer = document.querySelector('.horizontal-scroll-gallery');
+    const galleryWidth = galleryContainer.scrollWidth - galleryContainer.clientWidth;
+    const scrollPosition = galleryContainer.scrollLeft;
+
+    if (scrollPosition >= galleryWidth) {
+        resetGalleryScroll();
+    }
+}, 100);
