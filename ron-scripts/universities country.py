1d3d6e4b8e90ac1d523e7b8bfdffe25d21d7ebef
Ron Borenstein <122145739+ronborens@users.noreply.github.com>
2023-12-11 11:47:12 -0800

    Update universities country.py
    Switched from directly calling Nominatim API to using library geopy. Synchronous library for already async program, implemented threading. Shrunk bounding boxes.

--- ron-scripts/universities country.py
+++ ron-scripts/universities country.py
@@ -1,97 +1,72 @@
 import asyncio
 import aiohttp
 import json
-
-
-# NOTE: -- Test for single bounding box, no geopy, direct call async single thread
+from geopy.geocoders import Nominatim
+from concurrent.futures import ThreadPoolExecutor
 
 # Constants
+MAX_RETRIES = 3
 TIMEOUT = 10  # seconds
-REVERSE_GEOCODING_DELAY = 1
+
+geolocator = Nominatim(user_agent="university_locator")
 
 
-async def fetch_universities_in_bbox(session, south, west, north, east):
-    query = f"""
-    [out:json][timeout:180];
-    (
-      way["amenity"="university"]({south},{west},{north},{east});
-      rel["amenity"="university"]({south},{west},{north},{east});
-    );
-    out center;
-    >;
-    out skel qt;
-    """
-    headers = {"Content-Type": "text/plain"}
-    try:
-        async with session.post("http://overpass-api.de/api/interpreter", data=query, headers=headers, timeout=TIMEOUT) as response:
-            if response.content_type == 'application/json':
-                data = await response.json()
-                # Log fetched data
-                print(
-                    f"Data fetched for bbox {south},{west},{north},{east}: {data}")
-                return data
-            else:
-                content = await response.text()
-                print(
-                    f"Non-JSON response for bbox {south},{west},{north},{east}: {content}")
-                return None
-    except Exception as e:
-        print(f"Error in bbox {south},{west},{north},{east}: {e}")
+async def fetch_universities_in_bbox(session, south, west, north, east, retry=0):
+    if retry >= MAX_RETRIES:
+        print(f"Max retries reached for bbox {south},{west},{north},{east}")
         return None
 
 
-async def reverse_geocode(session, latitude, longitude, university_name):
-    url = f"https://nominatim.openstreetmap.org/reverse?format=json&lat={latitude}&lon={longitude}"
-    try:
-        async with session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
-                country = data.get('address', {}).get('country', 'Unknown')
-                # Detailed logging with university name
-                print(
-                    f"Reverse geocoding response for {university_name} ({latitude}, {longitude}): {country}")
-                return country
-            else:
-                print(
-                    f"Reverse geocoding failed for {university_name} ({latitude}, {longitude}): {response.status}")
-                return 'Unknown'
-    except Exception as e:
-        print(
-            f"Error in reverse geocoding for {university_name} ({latitude}, {longitude}): {e}")
-        return 'Unknown'
+async def reverse_geocode(executor, latitude, longitude):
+    def geocode():
+        try:
+            location = geolocator.reverse(
+                (latitude, longitude), exactly_one=True)
+            return location.address.split(",")[-1].strip() if location else "Unknown"
+        except Exception as e:
+            return "Unknown"
+
+    return await asyncio.get_event_loop().run_in_executor(executor, geocode)
 
 
 async def main():
+    bounding_boxes = [(lat, lon, lat + 15, lon + 15)
+                      for lat in range(-90, 90, 15) for lon in range(-180, 180, 15)]
     universities = []
+
+    executor = ThreadPoolExecutor(max_workers=10)
     async with aiohttp.ClientSession() as session:
-        # Single bounding box covering a part of the eastern United States, testing for country insertion
-        south, west, north, east = 40.0, -80.0, 45.0, -70.0
-        print(
-            f"Processing bounding box: South={south}, West={west}, North={north}, East={east}")
-        response = await fetch_universities_in_bbox(session, south, west, north, east)
-        if response and "elements" in response:
-            for element in response["elements"]:
-                if element["type"] == "way" or element["type"] == "relation":
-                    if "tags" in element and "name" in element["tags"]:
-                        latitude = element["center"]["lat"]
-                        longitude = element["center"]["lon"]
-                        university_name = element["tags"]["name"]
-                        # Delay to avoid rate limit
-                        # await asyncio.sleep(REVERSE_GEOCODING_DELAY)
-                        country = await reverse_geocode(session, latitude, longitude, university_name)
-                        universities.append({
-                            "name": element["tags"]["name"],
-                            "latitude": latitude,
-                            "longitude": longitude,
-                            "country": country
-                        })
-        else:
-            print("No data or error for the specified bounding box.")
+        for i, bbox in enumerate(bounding_boxes):
+            print(
+                f"Processing bounding box {i+1}/{len(bounding_boxes)}: {bbox}")
+            response = None
+            for attempt in range(MAX_RETRIES):
+                response = await fetch_universities_in_bbox(session, *bbox)
+                if response:
+                    break
+                print(
+                    f"Retrying bbox {bbox} (attempt {attempt + 2}/{MAX_RETRIES})...")
+                await asyncio.sleep(1)  # Delay before retrying
+
+            if response:
+                for element in response["elements"]:
+                    if element["type"] == "way" or element["type"] == "relation":
+                        if "tags" in element and "name" in element["tags"]:
+                            latitude = element["center"]["lat"]
+                            longitude = element["center"]["lon"]
+                            country = await reverse_geocode(executor, latitude, longitude)
+                            universities.append({
+                                "name": element["tags"]["name"],
+                                "latitude": latitude,
+                                "longitude": longitude,
+                                "country": country
+                            })
 
     json_output = json.dumps(universities, indent=4)
-    with open('universities_single_bbox.json', 'w') as file:
+    with open('universities_worldwide_async.json', 'w') as file:
         file.write(json_output)
 
-    print("Finished processing.")
+    print("Finished processing universities.")
+    executor.shutdown()
 
 asyncio.run(main())
