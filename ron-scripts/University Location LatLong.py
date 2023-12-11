b9d81e12ee5cd50900ca79cfb5ec83ed204a0702
Ron Borenstein <122145739+ronborens@users.noreply.github.com>
2023-12-11 11:50:42 -0800

    Update University Location LatLong.py
    switched back to direct calls, removed geopy and threads.
    
    --TODO: stalling at 20% add logging to find problem

--- ron-scripts/University Location LatLong.py
+++ ron-scripts/University Location LatLong.py
@@ -27,6 +27,21 @@ async def fetch_universities_in_bbox(session, south, west, north, east):
         print(f"Error in bbox {south},{west},{north},{east}: {e}")
         return None
 
+async def reverse_geocode(session, latitude, longitude):
+    url = f"https://nominatim.openstreetmap.org/reverse?format=json&lat={latitude}&lon={longitude}"
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                country = data.get('address', {}).get('country', 'Unknown')
+                return country
+            else:
+                print(f"Reverse geocoding failed: {response.status}")
+                return 'Unknown'
+    except Exception as e:
+        print(f"Error in reverse geocoding: {e}")
+        return 'Unknown'
+
 async def main():
     bounding_boxes = [(lat, lon, lat + 60, lon + 60)
                       for lat in range(-90, 90, 60) for lon in range(-180, 180, 60)]
@@ -40,10 +55,14 @@ async def main():
                 for element in response["elements"]:
                     if element["type"] == "way" or element["type"] == "relation":
                         if "tags" in element and "name" in element["tags"]:
+                            latitude = element["center"]["lat"]
+                            longitude = element["center"]["lon"]
+                            country = await reverse_geocode(session, latitude, longitude)
                             universities.append({
                                 "name": element["tags"]["name"],
-                                "latitude": element["center"]["lat"],
-                                "longitude": element["center"]["lon"]
+                                "latitude": latitude,
+                                "longitude": longitude,
+                                "country": country
                             })
 
     json_output = json.dumps(universities, indent=4)
