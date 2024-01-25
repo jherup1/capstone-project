abe6b093723de5d2e19a7d98ec23e486ef6090b6
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-25 02:32:58 +0000

    Ron: created website foundation (onboard pages, main home, firebase stuff)

---
+++ lib/components/side_bar_nav_model.dart
@@ -0,0 +1,19 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'side_bar_nav_widget.dart' show SideBarNavWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class SideBarNavModel extends FlutterFlowModel<SideBarNavWidget> {
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
