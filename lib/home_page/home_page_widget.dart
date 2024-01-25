a99939db6b4481d9fabe437c0f846f370630c0c8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-25 02:32:58 +0000

    Ron: created website foundation (onboard pages, main home, firebase stuff)

---
+++ lib/home_page/home_page_widget.dart
@@ -0,0 +1,194 @@
+import '/backend/backend.dart';
+import '/components/side_bar_nav_widget.dart';
+import '/flutter_flow/flutter_flow_static_map.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/lat_lng.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mapbox_search/mapbox_search.dart';
+import 'package:provider/provider.dart';
+import 'home_page_model.dart';
+export 'home_page_model.dart';
+
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({
+    Key? key,
+    this.location,
+  }) : super(key: key);
+
+  final DocumentReference? location;
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  late HomePageModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => HomePageModel());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isiOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Theme.of(context).brightness,
+          systemStatusBarContrastEnforced: true,
+        ),
+      );
+    }
+
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        body: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (responsiveVisibility(
+              context: context,
+              phone: false,
+              tablet: false,
+            ))
+              wrapWithModel(
+                model: _model.sideBarNavModel,
+                updateCallback: () => setState(() {}),
+                child: SideBarNavWidget(
+                  oneBG: FlutterFlowTheme.of(context).primaryBackground,
+                  oneIcon: Icon(
+                    Icons.bar_chart_rounded,
+                    color: FlutterFlowTheme.of(context).primary,
+                  ),
+                  twoBG: FlutterFlowTheme.of(context).secondaryBackground,
+                  twoIcon: Icon(
+                    Icons.school_outlined,
+                    color: FlutterFlowTheme.of(context).primaryText,
+                  ),
+                  threeColor: FlutterFlowTheme.of(context).secondaryBackground,
+                  threeIcon: Icon(
+                    Icons.account_circle_outlined,
+                    color: FlutterFlowTheme.of(context).primaryText,
+                  ),
+                ),
+              ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(16.0, 64.0, 0.0, 16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Dashboard',
+                            style: FlutterFlowTheme.of(context).headlineSmall,
+                          ),
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(
+                                0.0, 4.0, 0.0, 0.0),
+                            child: Text(
+                              'Nearby Universities',
+                              style: FlutterFlowTheme.of(context).bodySmall,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(12.0, 0.0, 12.0, 0.0),
+                      child: Wrap(
+                        spacing: 8.0,
+                        runSpacing: 8.0,
+                        alignment: WrapAlignment.start,
+                        crossAxisAlignment: WrapCrossAlignment.start,
+                        direction: Axis.horizontal,
+                        runAlignment: WrapAlignment.start,
+                        verticalDirection: VerticalDirection.down,
+                        clipBehavior: Clip.none,
+                        children: [
+                          StreamBuilder<LocationsRecord>(
+                            stream:
+                                LocationsRecord.getDocument(widget.location!),
+                            builder: (context, snapshot) {
+                              // Customize what your widget looks like when it's loading.
+                              if (!snapshot.hasData) {
+                                return Center(
+                                  child: SizedBox(
+                                    width: 50.0,
+                                    height: 50.0,
+                                    child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                        FlutterFlowTheme.of(context).primary,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }
+                              final containerLocationsRecord = snapshot.data!;
+                              return Container(
+                                height:
+                                    MediaQuery.sizeOf(context).height * 0.85,
+                                decoration: BoxDecoration(),
+                                child: FlutterFlowStaticMap(
+                                  location:
+                                      containerLocationsRecord.addressLatLong!,
+                                  apiKey:
+                                      'pk.eyJ1Ijoicm9uYm9yZW5zIiwiYSI6ImNscmwwajAyODBnMzkyaHBraTQ0MWZ0bXIifQ.NajwbOMlqgAtrPdKCpaoGg',
+                                  style: MapBoxStyle.Outdoors,
+                                  width: MediaQuery.sizeOf(context).width * 1.0,
+                                  height:
+                                      MediaQuery.sizeOf(context).height * 1.0,
+                                  fit: BoxFit.cover,
+                                  borderRadius: BorderRadius.circular(24.0),
+                                  zoom: 12,
+                                  tilt: 0,
+                                  rotation: 0,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
