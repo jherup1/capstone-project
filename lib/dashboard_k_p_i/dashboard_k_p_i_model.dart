d1cc50102e53a6f1dcafa3b96946a1de73543993
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-14 16:02:24 +0000

    Ron: WE FIXED IT BABYYYYY.

--- lib/dashboard_k_p_i/dashboard_k_p_i_model.dart
+++ lib/dashboard_k_p_i/dashboard_k_p_i_model.dart
@@ -1,8 +1,10 @@
+import '/backend/backend.dart';
 import '/components/breadcrumbs_header/breadcrumbs_header_widget.dart';
 import '/components/side_bar_nav/side_bar_nav_widget.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'dashboard_k_p_i_widget.dart' show DashboardKPIWidget;
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class DashboardKPIModel extends FlutterFlowModel<DashboardKPIWidget> {
   ///  State fields for stateful widgets in this page.
@@ -12,6 +14,13 @@ class DashboardKPIModel extends FlutterFlowModel<DashboardKPIWidget> {
   late SideBarNavModel sideBarNavModel1;
   // Model for BreadcrumbsHeader component.
   late BreadcrumbsHeaderModel breadcrumbsHeaderModel;
+  // State field(s) for ListView widget.
+
+  PagingController<DocumentSnapshot?, SupportStatsRecord>?
+      listViewPagingController1;
+  Query? listViewPagingQuery1;
+  List<StreamSubscription?> listViewStreamSubscriptions1 = [];
+
   // Model for sideBarNav component.
   late SideBarNavModel sideBarNavModel2;
 
@@ -28,6 +37,44 @@ class DashboardKPIModel extends FlutterFlowModel<DashboardKPIWidget> {
     unfocusNode.dispose();
     sideBarNavModel1.dispose();
     breadcrumbsHeaderModel.dispose();
+    for (var s in listViewStreamSubscriptions1) {
+      s?.cancel();
+    }
+    listViewPagingController1?.dispose();
+
     sideBarNavModel2.dispose();
+  }
+
+  /// Additional helper methods.
+  PagingController<DocumentSnapshot?, SupportStatsRecord>
+      setListViewController1(
+    Query query, {
+    DocumentReference<Object?>? parent,
+  }) {
+    listViewPagingController1 ??= _createListViewController1(query, parent);
+    if (listViewPagingQuery1 != query) {
+      listViewPagingQuery1 = query;
+      listViewPagingController1?.refresh();
+    }
+    return listViewPagingController1!;
+  }
+
+  PagingController<DocumentSnapshot?, SupportStatsRecord>
+      _createListViewController1(
+    Query query,
+    DocumentReference<Object?>? parent,
+  ) {
+    final controller = PagingController<DocumentSnapshot?, SupportStatsRecord>(
+        firstPageKey: null);
+    return controller
+      ..addPageRequestListener(
+        (nextPageMarker) => querySupportStatsRecordPage(
+          nextPageMarker: nextPageMarker,
+          streamSubscriptions: listViewStreamSubscriptions1,
+          controller: controller,
+          pageSize: 25,
+          isStream: true,
+        ),
+      );
   }
 }
