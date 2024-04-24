d1a2c9c6cc27a1de5faeba8fc634205b31933f0c
ronborens <rborenstein@nevada.unr.edu>
2024-04-24 01:09:33 -0700

    Add Firebase Analytics to web_plugin_registrant.dart and update dependencies in pubspec.yaml

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_performance_web/firebase_performance_web.dart';
@@ -27,6 +28,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   FirebaseFirestoreWeb.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebasePerformanceWeb.registerWith(registrar);
